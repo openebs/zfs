899a6d0ebc62a3606f5f3f9ef68bbed4a8e78f08
wangzihao <wangzihao18@huawei.com>
2019-11-21 20:29:26 +0800

    feat(build): add support for building on arm and x86
    Related to: openebs/openebs#1295
    - modify build_image.sh to decide which arch is used
    - add libkqueue deb which is not found in apt source.
    
    Signed-off-by: wangzihao <wangzihao18@huawei.com>

---
+++ docker/entrypoint-poolimage-arm64.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -o errexit
+trap 'call_exit $LINE_NO' EXIT
+
+call_exit()
+{
+echo "at call_exit.."     
+echo  "exit code:" $?
+echo "reference: "  $0 
+}
+
+
+if [ -z "$LOGLEVEL" ]; then
+	LOGLEVEL=info
+fi
+echo "sleeping for 2 sec"
+sleep 2
+export LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libjemalloc.so
+exec /usr/local/bin/zrepl -l $LOGLEVEL
